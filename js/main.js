c153ea625629ff7da24dd1110e66b76120c0752a
Mihail <mihail7enov@mail.ru>
2022-10-08 05:11:59 +0300

    task corrected

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 
-const randomize = (first, second) => first <= second ? Math.random(first, second) : 'Некорректный интервал';
-
-const commentChecker = (comment, maxLength) => comment <= maxLength;
+const getRandomInt = (first, second) => {
+  if (first < second) {
+    return Math.floor(first + Math.random() * (second + 1 - first));
+  }
+  throw new Error('Некорректный интервал');
+};
+getRandomInt();
+const checkStringLength = (value, maxLength) => value <= maxLength;
+checkStringLength();
